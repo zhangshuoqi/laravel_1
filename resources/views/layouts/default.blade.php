4e3231bd257db1d4fbbcf64f08c75042eeaa2e40
FOX <fox@FOX.local>
2018-06-27 17:15:11 +0800

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,10 +1,17 @@
 <!DOCTYPE html>
-
 <html>
-    <head>
-        <title>@yield('title','Sample') － Laravel 新手入门教程</title>
-    </head>
-    <body>
+<head>
+    <title>@yield('title', 'Sample App') - Laravel 入门教程</title>
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+@include('layouts._header')
+
+<div class="container">
+    <div class="col-md-offset-1 col-md-10">
         @yield('content')
-    </body>
+        @include('layouts._footer')
+    </div>
+</div>
+</body>
 </html>
