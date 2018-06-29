4eaa667f4754408a2e10825c2def8295abf39295
FOX <fox@FOX.local>
2018-06-30 01:09:13 +0800

    finished login and logout

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>@yield('title', 'Sample App') - Laravel 入门教程</title>
     <link rel="stylesheet" href="/css/app.css">
 </head>
+
 <body>
 @include('layouts._header')
 
@@ -14,5 +16,8 @@
         @include('layouts._footer')
     </div>
 </div>
+
+<script src="/js/app.js"></script>
+
 </body>
 </html>
